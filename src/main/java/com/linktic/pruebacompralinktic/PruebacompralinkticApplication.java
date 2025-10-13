ecb208086b4de41358874f3ed48af2cfb68b7c69
Carlos Steven Vargas Hernandez <steven17.v@gmail.com>
2025-10-13 12:49:39 -0500

    primer commit

---
+++ src/main/java/com/linktic/pruebacompralinktic/PruebacompralinkticApplication.java
@@ -0,0 +1,13 @@
+package com.linktic.pruebacompralinktic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebacompralinkticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebacompralinkticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linktic/pruebacompralinktic/PruebacompralinkticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktic.pruebacompralinktic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebacompralinkticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
