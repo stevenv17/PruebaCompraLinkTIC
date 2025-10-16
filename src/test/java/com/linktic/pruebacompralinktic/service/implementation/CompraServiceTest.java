41ee5f398546a769972c261d1f014a4e40f53394
Carlos Steven Vargas Hernandez <steven17.v@gmail.com>
2025-10-16 13:31:32 -0500

    Cobertura pruebas unitarias

--- src/test/java/com/linktic/pruebacompralinktic/service/implementation/CompraServiceTest.java
+++ src/test/java/com/linktic/pruebacompralinktic/service/implementation/CompraServiceTest.java
@@ -86,6 +86,33 @@ class CompraServiceTest {
   }
 
   @Test
+  void comprarProductoFallidoInesperadoProducto() {
+
+    String mensajeEx = "Error inesperado";
+    CompraProductoDto compraProductoDto = new CompraProductoDto();
+    compraProductoDto.setIdProducto(1);
+    compraProductoDto.setCantidad(10);
+
+    // Cuerpo JSON de error
+    String jsonError = "{\"codigo\":500,\"resultado\":\"ERROR\",\"mensaje\":\"Error inesperado\"}";
+
+    // Crea un FeignException simulado
+    FeignException feignException = mock(FeignException.class);
+    when(feignException.contentUTF8()).thenReturn(jsonError);
+
+    // consultaProducto
+    when(productoClient.obtenerProductoPorId(any(), eq(compraProductoDto.getIdProducto())))
+        .thenThrow(feignException);
+
+    ErrorGeneralException ex = assertThrows(ErrorGeneralException.class, () -> {
+      compraService.comprarProducto(compraProductoDto);
+    });
+
+    assertEquals(mensajeEx, ex.getMessage());
+
+  }
+
+  @Test
   void comprarProductoFallidoNoInventario() {
 
     String mensajeEx = "Inventario no encontrado";
@@ -119,5 +146,121 @@ class CompraServiceTest {
 
     assertEquals(mensajeEx, ex.getMessage());
 
+  }
+
+  @Test
+  void comprarProductoFallidoInesperadoInventario() {
+
+    String mensajeEx = "Error consultando inventario";
+    CompraProductoDto compraProductoDto = new CompraProductoDto();
+    compraProductoDto.setIdProducto(1);
+    compraProductoDto.setCantidad(10);
+
+    ProductoDtoOut productoDtoOut = new ProductoDtoOut();
+    productoDtoOut.setId(compraProductoDto.getIdProducto());
+    productoDtoOut.setNombre("Cuaderno 100 hojas");
+    productoDtoOut.setPrecio(5000L);
+
+    // Cuerpo JSON de error
+    String jsonError = "{\"codigo\":500,\"resultado\":\"ERROR\",\"mensaje\":\"\"Error consultando inventario\"}";
+
+    // Crea un FeignException simulado
+    FeignException feignException = mock(FeignException.class);
+    when(feignException.contentUTF8()).thenReturn(jsonError);
+
+    // consultaProducto
+    when(productoClient.obtenerProductoPorId(any(), eq(compraProductoDto.getIdProducto())))
+        .thenReturn(productoDtoOut);
+
+    // validarDisponibilidadProducto
+    when(inventarioClient.obtenerInventarioProducto(any(), eq(compraProductoDto.getIdProducto())))
+        .thenThrow(feignException);
+
+    ErrorGeneralException ex = assertThrows(ErrorGeneralException.class, () -> {
+      compraService.comprarProducto(compraProductoDto);
+    });
+
+    assertEquals(mensajeEx, ex.getMessage());
+
+  }
+
+  @Test
+  void comprarProductoFallidoActualizaInventario() {
+    String mensajeEx = "Inventario no encontrado";
+    CompraProductoDto compraProductoDto = new CompraProductoDto();
+    compraProductoDto.setIdProducto(1);
+    compraProductoDto.setCantidad(10);
+
+    ProductoDtoOut productoDtoOut = new ProductoDtoOut();
+    productoDtoOut.setId(compraProductoDto.getIdProducto());
+    productoDtoOut.setNombre("Cuaderno 100 hojas");
+    productoDtoOut.setPrecio(5000L);
+
+    InventarioDtoOut inventarioDtoOut = new InventarioDtoOut();
+    inventarioDtoOut.setCantidad(100);
+
+    // Cuerpo JSON de error
+    String jsonError = "{\"codigo\":404,\"resultado\":\"ERROR\",\"mensaje\":\"Inventario no encontrado\"}";
+
+    // Crea un FeignException simulado
+    FeignException feignException = mock(FeignException.class);
+    when(feignException.contentUTF8()).thenReturn(jsonError);
+
+    // consultaProducto
+    when(productoClient.obtenerProductoPorId(any(), eq(compraProductoDto.getIdProducto())))
+        .thenReturn(productoDtoOut);
+
+    // validarDisponibilidadProducto
+    when(inventarioClient.obtenerInventarioProducto(any(), eq(compraProductoDto.getIdProducto())))
+        .thenReturn(inventarioDtoOut);
+
+    // actualizarInventario
+    when(inventarioClient.actualizarCantidad(any(), any(ActualizaInventarioDto.class))).thenThrow(feignException);
+
+    ElementoNoEncontradoException ex = assertThrows(ElementoNoEncontradoException.class, () -> {
+      compraService.comprarProducto(compraProductoDto);
+    });
+
+    assertEquals(mensajeEx, ex.getMessage());
+  }
+
+  @Test
+  void comprarProductoFallidoInesperadoActualizaInventario() {
+    String mensajeEx = "Error inesperado";
+    CompraProductoDto compraProductoDto = new CompraProductoDto();
+    compraProductoDto.setIdProducto(1);
+    compraProductoDto.setCantidad(10);
+
+    ProductoDtoOut productoDtoOut = new ProductoDtoOut();
+    productoDtoOut.setId(compraProductoDto.getIdProducto());
+    productoDtoOut.setNombre("Cuaderno 100 hojas");
+    productoDtoOut.setPrecio(5000L);
+
+    InventarioDtoOut inventarioDtoOut = new InventarioDtoOut();
+    inventarioDtoOut.setCantidad(100);
+
+    // Cuerpo JSON de error
+    String jsonError = "{\"codigo\":500,\"resultado\":\"ERROR\",\"mensaje\":\"Error inesperado\"}";
+
+    // Crea un FeignException simulado
+    FeignException feignException = mock(FeignException.class);
+    when(feignException.contentUTF8()).thenReturn(jsonError);
+
+    // consultaProducto
+    when(productoClient.obtenerProductoPorId(any(), eq(compraProductoDto.getIdProducto())))
+        .thenReturn(productoDtoOut);
+
+    // validarDisponibilidadProducto
+    when(inventarioClient.obtenerInventarioProducto(any(), eq(compraProductoDto.getIdProducto())))
+        .thenReturn(inventarioDtoOut);
+
+    // actualizarInventario
+    when(inventarioClient.actualizarCantidad(any(), any(ActualizaInventarioDto.class))).thenThrow(feignException);
+
+    ErrorGeneralException ex = assertThrows(ErrorGeneralException.class, () -> {
+      compraService.comprarProducto(compraProductoDto);
+    });
+
+    assertEquals(mensajeEx, ex.getMessage());
   }
 }
